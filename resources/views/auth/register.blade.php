cc46a6978aee82f62cbe835cbbc11738791dc3af
TomoakiKaneko <tkaneko@brightwise.jp>
2019-07-03 10:24:30 +0000

    user login

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,22 +8,22 @@
     <div class="row">
         <div class="col-sm-6 offset-sm-3">
             
-            {!! Form::open(['route' => 'signup.post'] !!}
-                <div class="form-control">
+            {!! Form::open(['route' => 'signup.post']) !!}
+                <div class="form-group">
                     {!! Form::label('name', 'Name') !!}
                     {!! Form::text('name', old('name'), ['class' => 'form-control']) !!}
                 </div>
-                <div class="form-control">
+                <div class="form-group">
                     {!! Form::label('email', 'Email') !!}
-                    {!! Form::email('email', old('email'), ['class' => 'form-control'] !!}
+                    {!! Form::email('email', old('email'), ['class' => 'form-control']) !!}
                 </div>
-                <div class="form-control">
+                <div class="form-group">
                     {!! Form::label('password', 'Password') !!}
-                    {!! Form::password('password', ['class' => 'form-control'] !!}
+                    {!! Form::password('password', ['class' => 'form-control']) !!}
                 </div>
-                <div class="form-control">
+                <div class="form-group">
                     {!! Form::label('password_confirmation', 'Confirmation') !!}
-                    {!! Form::password('password_confirmation', ['class' => 'form-control'] !!}
+                    {!! Form::password('password_confirmation', ['class' => 'form-control']) !!}
                 </div>
                 
                 {!! Form::submit('submit', ['class' => 'btn btn-primary btn-block']) !!}
